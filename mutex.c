0344c077eeade41bb7be6d18b32d960dd40255a7
Jan Boerman <janboerman95@gmail.com>
2018-05-03 15:59:43 +0200

    it works

--- mutex.c
+++ mutex.c
@@ -1,240 +1,6 @@
 #include <sylvan.h>
-#include <stdio.h>
-#include <float.h>
-
-enum Var {
-    WAIT,
-    WAIT_PRIME,
-    CS,
-    CS_PRIME,
-    FINISHED,
-    FINISHED_PRIME
-};
-
-BDD makeMap() {
-    LACE_ME;
-
-    BDD map = sylvan_map_empty();
-    map = sylvan_map_add(map, WAIT_PRIME, sylvan_ithvar(WAIT));
-    map = sylvan_map_add(map, CS_PRIME, sylvan_ithvar(CS));
-    map = sylvan_map_add(map, FINISHED_PRIME, sylvan_ithvar(FINISHED));
-
-    return map;
-}
-
-BDD makeVariables() {
-    LACE_ME;
-
-    BDD set = sylvan_set_empty();
-    set = sylvan_set_add(set, WAIT);
-    set = sylvan_set_add(set, CS);
-    set = sylvan_set_add(set, FINISHED);
-
-    return set;
-}
-
-BDD and3(BDD one, BDD two, BDD three) {
-    LACE_ME;
-
-    BDD r = sylvan_and(one, two);
-    r = sylvan_and(r, three);
-    return r;
-}
-
-BDD and6(BDD one, BDD two, BDD three, BDD four, BDD five, BDD six) {
-    LACE_ME;
-
-    BDD x = and3(one, two, three);
-    BDD y = and3(four, five, six);
-
-    return sylvan_and(x, y);
-}
-
-BDD initialState() {
-    LACE_ME;
-
-    BDD wait        = sylvan_ithvar(WAIT);
-    BDD cs          = sylvan_nithvar(CS);
-    BDD finished    = sylvan_nithvar(FINISHED);
-
-    return and3(wait, cs, finished);
-}
-
-BDD enteredState() {
-    LACE_ME;
-
-    BDD wait        = sylvan_nithvar(WAIT);
-    BDD cs          = sylvan_ithvar(CS);
-    BDD finished    = sylvan_nithvar(FINISHED);
-
-    return and3(wait, cs, finished);
-}
-
-BDD exitedState() {
-    LACE_ME;
-
-    BDD wait        = sylvan_nithvar(WAIT);
-    BDD cs          = sylvan_nithvar(CS);
-    BDD finished    = sylvan_ithvar(FINISHED);
-
-    return and3(wait, cs, finished);
-}
-
-BDD leftState() {
-    LACE_ME;
-
-    BDD wait        = sylvan_nithvar(WAIT);
-    BDD cs          = sylvan_nithvar(CS);
-    BDD finished    = sylvan_nithvar(FINISHED);
-
-    return and3(wait, cs, finished);
-}
-
-BDD enter2(BDD set) {
-    LACE_ME;
-
-    BDD requirement_cs      = sylvan_nithvar(CS);
-    BDD requirement_wait    = sylvan_ithvar(WAIT);
-
-    BDD requirement_cs_prime    = sylvan_ithvar(CS_PRIME);
-    BDD requirement_wait_prime  = sylvan_nithvar(WAIT_PRIME);
-
-    BDD precondition    = sylvan_and(requirement_cs, requirement_wait);
-    BDD postcondition   = sylvan_and(requirement_cs_prime, requirement_wait_prime);
-
-    BDD relation = sylvan_and(precondition, postcondition);
-
-    BDD vars = sylvan_set_empty();
-    vars = sylvan_set_add(vars, CS);
-    vars = sylvan_set_add(vars, WAIT);
-
-    BDD exists = sylvan_exists(sylvan_and(set, relation), vars);
-    BDD enter = sylvan_compose(exists, makeMap());
-    return enter;
-}
-
-// BDD enterRelation(BDD set, BDD vars) {
-//     LACE_ME;
-
-//     BDD requirement_cs      = sylvan_nithvar(CS);
-//     BDD requirement_wait    = sylvan_ithvar(WAIT);
-
-//     BDD requirement_cs_prime    = sylvan_ithvar(CS_PRIME);
-//     BDD requirement_wait_prime  = sylvan_nithvar(WAIT_PRIME);
-
-//     BDD precondition    = sylvan_and(requirement_cs, requirement_wait);
-//     BDD postcondition   = sylvan_and(requirement_cs_prime, requirement_wait_prime);
-
-//     BDD relation = sylvan_and(precondition, postcondition);
-//     return sylvan_exists(sylvan_and(set, relation), vars);
-// }
-
-BDD exit2(BDD set) {
-    LACE_ME;
-
-    BDD requirement_cs      = sylvan_ithvar(CS);
-
-    BDD requirement_cs_prime        = sylvan_nithvar(CS_PRIME);
-    BDD requirement_finished_prime  = sylvan_ithvar(FINISHED_PRIME);
-
-    BDD precondition  = requirement_cs;
-    BDD postcondition = sylvan_and(requirement_cs_prime, requirement_finished_prime);
-
-    BDD relation = sylvan_and(precondition, postcondition);
-
-    BDD vars = sylvan_set_empty();
-    vars = sylvan_set_add(vars, CS);
-    vars = sylvan_set_add(vars, FINISHED);
-
-    BDD exists = sylvan_exists(sylvan_and(set, relation), vars);
-    BDD exit = sylvan_compose(exists, makeMap());
-    return exit;
-}
-
-// BDD exitRelation(BDD set, BDD vars) {
-//     LACE_ME;
-
-//     BDD requirement_cs      = sylvan_ithvar(CS);
-
-//     BDD requirement_cs_prime        = sylvan_nithvar(CS_PRIME);
-//     BDD requirement_finished_prime  = sylvan_ithvar(FINISHED_PRIME);
-
-//     BDD precondition  = requirement_cs;
-//     BDD postcondition = sylvan_and(requirement_cs_prime, requirement_finished_prime);
-
-//     BDD relation = sylvan_and(precondition, postcondition);
-//     return sylvan_exists(sylvan_and(set, relation), vars);
-// }
-
-BDD leave2(BDD set) {
-    LACE_ME;
-
-    BDD postcondition = sylvan_nithvar(CS_PRIME);
-
-    BDD vars = sylvan_set_empty();
-    vars = sylvan_set_add(vars, CS);
-
-    BDD exists = sylvan_exists(sylvan_and(set, postcondition), vars);
-    BDD leave = sylvan_compose(exists, makeMap());
-    return leave;
-}
-
-// BDD leaveRelation(BDD set, BDD vars) {
-//     LACE_ME;
-
-//     BDD postcondition = sylvan_nithvar(CS_PRIME);
-
-//     return sylvan_exists(sylvan_and(set, postcondition), vars);
-// }
-
-BDD restart2(BDD set) {
-    LACE_ME;
-
-    BDD requirement_finished        = sylvan_ithvar(FINISHED);
-
-    BDD requirement_wait_prime      = sylvan_ithvar(WAIT_PRIME);
-    BDD requirement_finished_prime  = sylvan_nithvar(FINISHED_PRIME);
-
-    BDD precondition = requirement_finished;
-    BDD postcondition = sylvan_and(requirement_wait_prime, requirement_finished_prime);
-
-    BDD relation = sylvan_and(precondition, postcondition);
-
-    BDD vars = sylvan_set_empty();
-    vars = sylvan_set_add(vars, FINISHED);
-    vars = sylvan_set_add(vars, FINISHED_PRIME);
-
-    BDD exists = sylvan_exists(sylvan_and(set, relation), vars);
-    BDD restart = sylvan_compose(exists, makeMap());
-    return restart;
-}
-
-// BDD restartRelation(BDD set, BDD vars) {
-//     LACE_ME;
-
-//     BDD requirement_finished        = sylvan_ithvar(FINISHED);
-
-//     BDD requirement_wait_prime      = sylvan_ithvar(WAIT_PRIME);
-//     BDD requirement_finished_prime  = sylvan_nithvar(FINISHED_PRIME);
-
-//     BDD precondition = requirement_finished;
-//     BDD postcondition = sylvan_and(requirement_wait_prime, requirement_finished_prime);
-
-//     BDD relation = sylvan_and(precondition, postcondition);
-//     return sylvan_exists(sylvan_and(set, relation), vars);
-// }
-
-const int RELATION_COUNT = 4;
-BDD (*p[4]) (BDD set);
 
 int main() {
-    //initialize function pointer array (the relations)
-    p[0]    = enter2;
-    p[1]    = exit2;
-    p[2]    = leave2;
-    p[3]    = restart2;
-
-
     int n_workers = 0; // auto-detect
     lace_init(n_workers, 40960000);
     lace_startup(0, NULL, NULL);
@@ -244,54 +10,121 @@ int main() {
     sylvan_init_package();
     sylvan_init_mtbdd();
 
-    /* TODO ... do stuff ... */
-    LACE_ME;
+    LACE_ME
 
-    BDD variables       = makeVariables();  sylvan_protect(&variables);
-    
-    BDD initState       = initialState();   sylvan_protect(&initState);
-    BDD enterState      = enteredState();   sylvan_protect(&enterState);
-    BDD exitState       = exitedState();    sylvan_protect(&exitState);
-    BDD leaveState      = leftState();      sylvan_protect(&leaveState);
+    int CS = 0;
+    int CS_PRIME = 1;
+    int WAIT = 2;
+    int WAIT_PRIME = 3;
+    int FINISHED = 4;
+    int FINISHED_PRIME = 5;
 
-    double count = sylvan_satcount(initState, variables);
-    printf("initState satcount = %f\r\n", count);
+    int NUM_RELATIONS = 4;
 
-    //TODO implement algorithm to generate state-space o.0
-    BDD Vold = sylvan_set_empty();
-    BDD Vnew = sylvan_set_add(sylvan_set_empty(), initState);
+    BDD init = sylvan_nithvar(CS);
+    sylvan_protect(&init);
+    init = sylvan_and(init, sylvan_ithvar(WAIT));
+    init = sylvan_and(init, sylvan_nithvar(FINISHED));
 
-    sylvan_protect(&Vold);
-    sylvan_protect(&Vnew);
+    BDD map = sylvan_map_empty();
+    sylvan_protect(&map);
+    map = sylvan_map_add(map, CS_PRIME, sylvan_ithvar(CS));
+    map = sylvan_map_add(map, WAIT_PRIME, sylvan_ithvar(WAIT));
+    map = sylvan_map_add(map, FINISHED_PRIME, sylvan_ithvar(FINISHED));
 
-    while (&Vold != &Vnew) {
-        Vold = Vnew;
+    BDD vOld = sylvan_set_empty();
+    sylvan_protect(&vOld);
 
-        int i = 0;
-        for (i; i < RELATION_COUNT; i++) {
-            BDD newReachableStates = (*p[i]) (Vnew);
-            Vnew = sylvan_or(Vnew, newReachableStates);
+    BDD vNew = init;
+    sylvan_protect(&vNew);
+
+    BDD rels[NUM_RELATIONS];
+    BDD vars[NUM_RELATIONS];
+
+    // enter relation
+
+    BDD enter_pre = sylvan_and(sylvan_nithvar(CS), sylvan_ithvar(WAIT));
+    sylvan_protect(&enter_pre);
+    BDD enter_post = sylvan_and(sylvan_ithvar(CS_PRIME), sylvan_nithvar(WAIT_PRIME));
+    sylvan_protect(&enter_post);
+
+    BDD enter_vars = sylvan_set_empty();
+    sylvan_protect(&enter_vars);
+    enter_vars = sylvan_set_add(enter_vars, CS);
+    enter_vars = sylvan_set_add(enter_vars, WAIT);
+
+    rels[0] = sylvan_and(enter_pre, enter_post);
+    vars[0] = enter_vars;
+
+    // exit relation
+
+    BDD exit_pre = sylvan_ithvar(CS);
+    sylvan_protect(&exit_pre);
+    BDD exit_post = sylvan_and(sylvan_nithvar(CS_PRIME), sylvan_ithvar(FINISHED_PRIME));
+    sylvan_protect(&exit_post);
+
+    BDD exit_vars = sylvan_set_empty();
+    sylvan_protect(&exit_vars);
+    exit_vars = sylvan_set_add(exit_vars, CS);
+    exit_vars = sylvan_set_add(exit_vars, FINISHED);
+
+    rels[1] = sylvan_and(exit_pre, exit_post);
+    vars[1] = exit_vars;
+
+    // leave relation
+
+    BDD leave_pre = sylvan_set_empty();
+    sylvan_protect(&leave_pre);
+    BDD leave_post = sylvan_nithvar(CS_PRIME);
+    sylvan_protect(&leave_post);
+
+    BDD leave_vars = sylvan_set_empty();
+    sylvan_protect(&leave_vars);
+    leave_vars = sylvan_set_add(leave_vars, CS);
+
+    rels[2] = sylvan_and(leave_pre, leave_post);
+    vars[2] = leave_vars;
+
+    // restart relation
+
+    BDD restart_pre = sylvan_ithvar(FINISHED);
+    sylvan_protect(&restart_pre);
+    BDD restart_post = sylvan_and(sylvan_ithvar(WAIT_PRIME), sylvan_nithvar(FINISHED_PRIME));
+    sylvan_protect(&restart_post);
+
+    BDD restart_vars = sylvan_set_empty();
+    sylvan_protect(&restart_vars);
+    restart_vars = sylvan_set_add(restart_vars, WAIT);
+    restart_vars = sylvan_set_add(restart_vars, FINISHED);
+
+    rels[3] = sylvan_and(restart_pre, restart_post);
+    vars[3] = restart_vars;
+
+    while (vOld != vNew) {
+        printf("got here\n");
+
+        vOld = vNew;
+
+        int i;
+        for (i = 0; i < NUM_RELATIONS; i++) {
+            BDD relprod = sylvan_exists(sylvan_and(vNew, rels[i]), vars[i]);
+            relprod = sylvan_compose(relprod, map);
+
+            vNew = sylvan_or(vNew, relprod);
+
+            printf("Did this %d times\n", i);
         }
     }
 
-    count = sylvan_satcount(Vnew, variables);
-    printf("result satcount = %f\r\n", count);
+    //debug
+    int count = mtbdd_satcount(vNew, 3);
+    printf("SAT count: %d\n", count);
 
-    //TODO look at sylvan_protect and sylvan_unprotect
-    //TODO uses pointers to BDDs!
-    //TODO use sylvan_compose? where? how? it's used for function composition?
+    FILE *f = fopen("test.dot", "w+");
+    sylvan_fprintdot(f, vNew);
+    fclose(f);
 
-    sylvan_unprotect(&Vold);
-    sylvan_unprotect(&Vnew);
-
-    sylvan_unprotect(&initState);
-    sylvan_unprotect(&enterState);
-    sylvan_unprotect(&exitState);
-    sylvan_unprotect(&leaveState);
-
-    sylvan_stats_report(stderr);
+    sylvan_stats_report(stdout);
     sylvan_quit();
     lace_exit();
-
-    return 0;
 }
